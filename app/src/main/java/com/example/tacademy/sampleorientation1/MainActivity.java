6ae954cc1bf2b4dd9d0b194af233af4d902ee55f
kiss9815 <kiss9815@gmail.com>
2016-01-29 19:51:49 +0900

    complete

--- app/src/main/java/com/example/tacademy/sampleorientation1/MainActivity.java
+++ app/src/main/java/com/example/tacademy/sampleorientation1/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.tacademy.sampleorientation1;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        messageView = (TextView)findViewById(R.id.text_message);
+        Button btn = (Button)findViewById(R.id.btn_change);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageView.setText("change text");
+            }
+        });
+
+        Configuration config = getResources().getConfiguration();
+        if(config.orientation ==Configuration.ORIENTATION_PORTRAIT ){  //onConfigurationChanged 를 설정하면 처음 액티비티가 실행될때만 onCreate 가 실행됨
+            Toast.makeText(this, "onCreate.... Portrait", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "onCreate.... Landscape", Toast.LENGTH_SHORT).show();
+        }
+
+
+        String[] rotateMessage = {"0", "90" , "180" , "270"};
+        Display display = getWindowManager().getDefaultDisplay();    // display도 오리엔테이션 변경을 알 수 있다. 모든 방향에 대한 회전을 알 수 있음
+        int rotate = display.getRotation();
+        switch (rotate){
+            case Surface.ROTATION_0:
+            case Surface.ROTATION_90:
+            case Surface.ROTATION_180:
+            case Surface.ROTATION_270:
+                Toast.makeText(this, "rotate : " + rotateMessage[rotate] , Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) { // 오리엔테이션이 변경되어도 액티비티가 다시 실행되지 않도록 메니페스트에서 설정을 하면
+                                                                        // orientation이 변경될시 이 메소드가 호출됨
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "onConfigurationChanged.... Portrait", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "onConfigurationChanged.... Landscape", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
